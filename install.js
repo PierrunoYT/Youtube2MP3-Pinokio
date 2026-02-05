cd3f481189c86a76327f2705896d7cf603904db2
PierrunoYT <pierrebruno@hotmail.ch>
2026-02-05 16:45:37 +0100

    Add git clone command to install script for YouTube to MP3 app

--- install.js
+++ install.js
@@ -3,7 +3,16 @@ module.exports = {
     {
       method: "shell.run",
       params: {
+        message: [
+          "git clone https://github.com/PierrunoYT/Youtube2MP3-Pinokio app"
+        ]
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
         venv: "env",
+        path: "app",
         message: [
           "uv pip install -r requirements.txt"
         ]
